054b4509cd6456a30674a35d1d73421ea5baa1ce
ArdaT <127962996+MaymunAb@users.noreply.github.com>
2024-12-07 21:31:28 +0300

    Create Button.vue

---
+++ components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="custom-button" @click="handleClick">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: 'Click me'
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('clicked');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.custom-button {
+  padding: 10px 20px;
+  background-color: #3498db;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.custom-button:hover {
+  background-color: #2980b9;
+}
+</style>
