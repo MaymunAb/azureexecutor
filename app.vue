a779dec4f5211702725c18e6e7a3cea663d57c54
ArdaT <127962996+MaymunAb@users.noreply.github.com>
2024-12-07 21:37:31 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,26 +1,13 @@
 <template>
-  <div>
-    <h1>Welcome to my Nuxt App!</h1>
-    <CustomButton label="Click Me" @clicked="handleClick"/>
-  </div>
+  <AzureExecutor />
 </template>
 
 <script>
-// Import the CustomButton component
-import CustomButton from '~/components/CustomButton.vue'
+import AzureExecutor from '~/components/AzureExecutor.vue'
 
 export default {
   components: {
-    CustomButton
-  },
-  methods: {
-    handleClick() {
-      alert('Button clicked!');
-    }
+    AzureExecutor
   }
 }
 </script>
-
-<style>
-/* You can add global styles here or use scoped styles in individual components */
-</style>
