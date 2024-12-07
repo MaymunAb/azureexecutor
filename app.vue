524b291f3d0a82f63265749266ce3e9c4dac933d
ArdaT <127962996+MaymunAb@users.noreply.github.com>
2024-12-07 21:30:51 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,4 +1,26 @@
 <template>
   <div>
+    <h1>Welcome to my Nuxt App!</h1>
+    <CustomButton label="Click Me" @clicked="handleClick"/>
   </div>
 </template>
+
+<script>
+// Import the CustomButton component
+import CustomButton from '~/components/CustomButton.vue'
+
+export default {
+  components: {
+    CustomButton
+  },
+  methods: {
+    handleClick() {
+      alert('Button clicked!');
+    }
+  }
+}
+</script>
+
+<style>
+/* You can add global styles here or use scoped styles in individual components */
+</style>
